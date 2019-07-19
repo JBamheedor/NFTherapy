5e8343a4cf5a781373850778c5877f0fbdbfe9fe
Josh Bromehead <bromehead@lewiscommunications.com>
2019-07-19 14:55:54 -0500

    Change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,11 @@ module.exports = {
     ],
     link: [
       {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: 'favicon.ico'      
+      },
+      {
         rel: 'stylesheet',
         href:
           // 'https://fonts.googleapis.com/css?family=Dosis:200,300,400,500|Libre+Franklin&display=swap'
