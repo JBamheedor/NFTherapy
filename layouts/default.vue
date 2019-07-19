379e8408596f5433aba66b246be73c0e4213271f
Josh Bromehead <bromehead@lewiscommunications.com>
2019-07-19 15:18:49 -0500

    Add og:image for link sharing

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,11 +9,19 @@
 <script>
 import PageNavigation from '@/components/PageNavigation.vue'
 import PageFooter from '@/components/PageFooter.vue'
+import ogImage from '@/static/images/wood-path.jpg';
 
 export default {
   components: {
     PageNavigation,
     PageFooter
+  },
+  head () {
+    return {
+      meta: [
+        { hid: 'og:image', property: 'og:image', content: this.BASE_URL+ ogImage }
+      ]
+    }
   }
 }
 </script>
