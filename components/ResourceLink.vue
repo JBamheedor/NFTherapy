48b46ad5d6ce952267764fcf698b4683838d7c45
Josh <jmbromehead@gmail.com>
2019-08-27 13:50:04 -0500

    Page updates

---
+++ components/ResourceLink.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="resource-link">
+    <i :class="iconClass" />
+    <a class="link-item" :href="link" target="_blank">{{ linkTitle }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      default: ''
+    },
+    linkTitle: {
+      type: String,
+      default: ''
+    },
+    iconClass: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+svg {
+  color: $primary-green;
+  margin-right: 0.5rem;
+  width: 0.5rem !important;
+}
+
+.link-item {
+  color: #4a4a4a;
+  &:hover {
+    color: $dark-green;
+  }
+}
+</style>
---
+++ components/VideoSection.vue
@@ -0,0 +1,38 @@
+
+
+<template>
+  <div class="media-container">
+    <iframe
+      class="video"
+      width="560"
+      height="315"
+      :src="videoLink"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    videoLink: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.media-container {
+  display: flex;
+  justify-content: center;
+  padding-bottom: 4rem;
+}
+
+.video {
+  height: 315px;
+}
+</style>
